fd384193c8071dbd4bdb737c53e6dc0b0d70b150
Cristiana2000 <102221965+Cristiana2000@users.noreply.github.com>
2022-06-01 11:02:49 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let trains = [];
 
 function preload() {
   uImg = loadImage('sapo.png');
-  tImg = loadImage('train.png');
+  tImg = loadImage('lixo.png');
   bImg = loadImage('background.jpg');
 }
 
